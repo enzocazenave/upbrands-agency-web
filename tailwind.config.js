d55b12eab6a2928d7a13591ac4864d182c489a55
enzocazenave <enzocazenavedev@hotmail.com>
2024-05-25 16:44:25 -0300

    Add code

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat']
+      },
+      colors: {
+        magenta: '#E5007E',
+        gray: '#1D1D1B'
+      }
+    },
+  },
+  plugins: [],
+}
